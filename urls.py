9d984300a9d3a110e09d38d4bfb65fceb0d99271
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-02-25 20:55:30 -0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('team/', views.team, name='team'),
     path('terms/', views.terms, name='terms'),
