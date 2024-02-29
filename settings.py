8e7d97c58bb76c385cfee0bc3670b52c48f02675
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-02-29 00:13:11 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'kigalicoding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates/kigalicoding')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
